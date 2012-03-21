9b4a716151c3424298b342f8167cf5854f0644c4
Greg Albrecht <gba@splunk.com>
2012-03-21 09:10:00 -0700

    Added initial usage info.

--- pydstat.py
+++ pydstat.py
@@ -4,6 +4,15 @@
 A Pythonic wrapper for pidstat:
 http://manpages.ubuntu.com/manpages/lucid/en/man1/pidstat.1.html
 
+Usage
+=====
+  There are at least three possible ways to use this script.
+
+    1. Add as custom plugin for your Cloudkick agent (TK).
+    2. Create crontab to run periodically:
+      */5 * * * * /usr/local/bin/pydstat.py
+    3. Add as Splunk scripted input (TK).
+
 License
 =======
   Copyright 2011 Splunk, Inc.
