5ba99058c29d81254fe68d7d5f0f3b4f2ce94b90
Greg Albrecht <gba@splunk.com>
2012-08-15 14:16:35 -0700

    adding tests for real this time

---
+++ tests/context.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for pydstat.
+
+Source:: https://github.com/ampledata/pydstat
+"""
+
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
+import pydstat
